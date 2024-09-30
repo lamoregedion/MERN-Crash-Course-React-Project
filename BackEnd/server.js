65d5505958b838ed616e021b125eb74883ca462a
Gedion Abera <gedionable2@gmail.com>
2024-10-01 00:00:06 +0300

    Fix server.js route handler in backend

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,2 +1,8 @@
 // This is the entry point for all our APIs. Some might say it app.js, index.js or main.js, but all these are just a conventions.
 import express from 'express';
+
+const app = express();
+
+app.listen(5000, () => {
+    console.log('Server started at http://localhost:5000');
+})
