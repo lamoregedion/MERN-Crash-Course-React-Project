13a7df27238592aee4bf7098c56f80688cf514f5
Gedion Abera <gedionable2@gmail.com>
2024-10-04 23:10:31 +0300

    Fix server.js route handler in backend

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,6 +3,10 @@ import express from 'express';
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Server is ready12345");
+})
+
 app.listen(5000, () => {
     console.log('Server started at http://localhost:5000');
-})
+});
